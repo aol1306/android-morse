e191cd7eeda62eff0c225527d9b08ebe4a805475
aol1306 <jdzieciatko@gmail.com>
2017-02-08 05:23:17 +0000

    Initial commit

---
+++ morse.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3
+import time
+import logging
+import sys
+import os
+
+# only set dot length
+DOT = 0.03
+DASH = 3*DOT
+
+
+CODE = {
+'a' : '.-',
+'b' : '-...',
+'c' : '-.-.',
+'d' : '-..',
+'e' : '.',
+'f' : '..-.',
+'g' : '--.',
+'h' : '....',
+'i' : '..',
+'j' : '.---',
+'k' : '-.-',
+'l' : '.-..',
+'m' : '--',
+'n' : '-.',
+'o' : '---',
+'p' : '.--.',
+'q' : '--.-',
+'r' : '.-.',
+'s' : '...',
+'t' : '-',
+'u' : '..-',
+'v' : '...-',
+'w' : '.--',
+'x' : '-..-',
+'y' : '-.--',
+'z' : '--..',
+'.' : '.-.-.-',
+',' : '--..--',
+'?' : '..--..',
+'/' : '-..-.',
+'@' : '.--.-.',
+'1' : '.----',
+'2' : '..---',
+'3' : '...--',
+'4' : '....-',
+'5' : '.....',
+'6' : '-....',
+'7' : '--...',
+'8' : '---..',
+'9' : '----.',
+'0' : '-----',
+}
+
+logging.basicConfig(level=logging.DEBUG)
+
+def dot():
+    logging.debug("dot")
+    logging.info("singal on")
+    ret = os.system('termux-vibrate -f -d '+str(DOT*1000))
+    if ret != 0:
+        raise RuntimeError ("Install termux-api first!")
+    time.sleep(DOT)
+    logging.info("singal off")
+
+def dash():
+    logging.debug("dash")
+    logging.info("singal on")
+    os.system('termux-vibrate -f -d '+str(DASH*1000))
+    if ret != 0:
+        raise RuntimeError ("Install termux-api first!")
+    time.sleep(DASH)
+    logging.info("singal off")
+
+def part_of_letter_sleep():
+    logging.debug("part of letter sleep")
+    time.sleep(DOT)
+
+def letter_sleep():
+    logging.debug("letter sleep")
+    time.sleep(DASH)
+
+def word_sleep(): 
+    logging.debug("word sleep")
+    time.sleep(DOT*7)
+
+def transmit_letter(letter):
+    letter = letter.lower()
+    logging.debug("transmitting letter "+letter)
+    try:
+        code = CODE[str(letter)]
+    except KeyError:
+        logging.error("no such letter")
+        return
+    size = len(code)
+    i = 1
+    for sign in code:
+        if sign == '.':
+            dot()
+        if sign == '-':
+            dash()
+        if (i<size):
+            part_of_letter_sleep()
+        i += 1
+
+def transmit_word(word):
+    size = len(word)
+    i = 1
+    for letter in word:
+        transmit_letter(letter)
+        if (i<size):
+            letter_sleep()
+        i += 1
+
+def transmit(text):
+    text = text.split()
+    size = len(text)
+    i = 1
+    for word in text:
+        transmit_word(word)
+        if (i<size):
+            word_sleep()
+        i += 1
+
+if __name__ == "__main__":
+    try:
+        transmit(sys.argv[1])
+    except IndexError:
+        quit("usage: ./morse.py '[text]'")
